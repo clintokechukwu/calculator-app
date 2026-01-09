406c36360b8a2c7fe7dfc4ec30b5ad15e8c23276
cokech4 <cokech4@wgu.edu>
2026-01-09 05:39:38 -0600

    divide by zero logic fixed and some comments added

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ display.value = ''
 
 let problemSolved = false;
 
-function solveMath () {
+function solveProblem () {
   let answer = null
   if (number1 && number2) {
     if (operator === '+') {
@@ -22,17 +22,18 @@ function solveMath () {
     } else if (operator === '*') {
       answer = Number(number1) * Number(number2)
     } else if (operator === '/') {
-      answer = Number(number1) / Number(number2)
+      if (Number(number2) !== 0) {
+        answer = Number(number1) / Number(number2)
+      } else {
+        answer = 'error'
+      }
     } 
   } else {
     answer = 'error';
   }
+  
   problemSolved = true;
-  display.value = ''
   display.value = answer;
-  number1 = '';
-  number2 = '';
-  operator = '';
 }
 
 
@@ -41,19 +42,37 @@ function solveMath () {
 // numbers click logic
 for (let i = 0; i < keypad.length; i++) {
   keypad[i].addEventListener('click', function () {
+
+    // check to see if a calculation was made and resets 
     if (problemSolved) {
-      display.value = '';
       problemSolved = false;
+      display.value = '';
+      number1 = '';
+      number2 = '';
+      operator = '';
     }
+
+    // decimal logic - allows only one decimal per number
+    if (keypad[i].value === '.' && number1.includes('.') && !operator) {
+      return;
+    }
+    if (keypad[i].value === '.' && number2.includes('.') && operator) {
+      return;
+    }
+
+
+    // keypad logic - logs numbers and operators to desired variable and displays it to screen
     if (keypad[i].value !== 'delete' && keypad[i].value !== 'clear' ) {
       if (number1.length <= 9 && !operator) {
-        number1 += keypad[i].value 
+        number1 += keypad[i].value
         display.value += keypad[i].value
       } else if (number2.length <= 9 && operator) {
         number2 += keypad[i].value 
         display.value += keypad[i].value
       }
-    } 
+    }
+
+    // keypad logic - delete button removes the last character from string and screen
     if (keypad[i].value === 'delete') {
       console.log('You pressed delete');
       if (number2) {
@@ -65,11 +84,18 @@ for (let i = 0; i < keypad.length; i++) {
       }
       display.value = display.value.slice(0, -1);
     }
+
+    // keypad logic - clear button clears all variables and screen output
     if (keypad[i].value === 'clear') {
       number1 = '';
       number2 = '';
       operator = '';
       display.value = ''
+    }
+
+    // alert to new features yet to be added
+    if (keypad[i].textContent === '+/-' || keypad[i].textContent === '%') {
+      alert('Feature coming soon...')
     }
     
   });
@@ -96,5 +122,5 @@ for (let i = 0; i < keypadOperators.length; i++) {
 
 
 // solve event 
-equal.addEventListener('click', solveMath);
+equal.addEventListener('click', solveProblem);
 
